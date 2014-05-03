84370fa266622b8d75a1b8467e8ee3ac5e6b2df1
Aaron Roberts <lilasquared@gmail.com>
2014-05-03 16:51:45 -0400

    Renamed Worksheet to Template

--- application/views/template/view.php
+++ application/views/template/view.php
@@ -1,7 +1,7 @@
 <div class="row-fluid">
   <?php foreach ($templates as $template) : ?>
     <div class="col-xs-4 col-md-3">
-      <a href="/Worksheet/Edit/<?php echo $template->templateId; ?>" class="thumbnail template">
+      <a href="/Template/Edit/<?php echo $template->templateId; ?>" class="thumbnail template">
         <?php echo $template->title; ?>
       </a>
     </div>
